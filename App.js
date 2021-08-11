a41f2dac7fa54d32155c3b01855f601edbc1903b
GOPICHAND Ravikanti <ravikanti.gopichand@soprasteria.com>
2021-08-11 13:19:47 +0530

    With basics effects and displays the name on page

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UseEffectBasics from './tutorial/UseEffectBasics';
+
+
+function App() {
+  return (<div className='container'>
+    <h2>Advance React js</h2>
+
+    <UseEffectBasics />
+  </div>
+  );
+}
+
+export default App;
