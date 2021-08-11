c956c9a13b2f46819100642b0bf587653fe786f3
GOPICHAND Ravikanti <ravikanti.gopichand@soprasteria.com>
2021-08-11 12:01:16 +0530

    simple counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import UseStateCounter from "./tutorial/UseStateCounter";
 //import UseStateErrorEx from "./tutorial/useState-objects.js";
-import UseStateBasic from './tutorial/UseStateBasic';
+//import UseStateBasic from './tutorial/UseStateBasic';
+//import UseStateArray from "./tutorial/UseStateArray";
+//import UseStateObjects from "./tutorial/useStateObjects";
 function App() {
   return (
     <div className="container">
       <h1>Advance React JS</h1>
-      <UseStateBasic />
+      <UseStateCounter />
     </div>
   );
 }
