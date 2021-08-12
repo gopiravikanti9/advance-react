e36ca895960cc2abbf4b384a17792c711096e9f3
gopiravikanti9 <58160743+gopiravikanti9@users.noreply.github.com>
2021-08-12 14:54:18 +0530

    Working with reducer

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Forms from "./Tutorial/Forms";
 import LearnJSON from "./Tutorial/LearnJSON";
+import Index from "./Tutorial/Reducer";
 import ToDo from "./Tutorial/ToDo";
 import UseEffectsBasics from "./Tutorial/UseEffectsBasics";
 import UseStateArray from "./Tutorial/UseStateArray";
-import UseStateBasic from "./Tutorial/UseStateBasic"; import UseStateCounter from "./Tutorial/UseStateCounter";
-import UseStateExample from "./Tutorial/useStateExample";
+import UseStateBasic from "./Tutorial/UseStateBasic";import UseStateCounter from "./Tutorial/UseStateCounter";
+ import UseStateExample from "./Tutorial/useStateExample";
 import UseStateObject from "./Tutorial/UseStateObject";
 function App() {
   return (
     <div className="container">
       <header className="App-header">
-        <h1>Advance React</h1>
+      <h1>Advance React</h1>
         {/* <UseStateExample/> */}
         {/* <UseStateBasic/> */}
         {/* <UseStateArray/> */}
@@ -20,12 +21,11 @@ function App() {
         {/* <UseEffectsBasics/> */}
         {/* <Forms/> */}
         {/* <ToDo/> */}
-
-        <LearnJSON />
+        {/* <LearnJSON/> */}
+        <Index/>
       </header>
     </div>
   );
 }
 
 export default App;
-
