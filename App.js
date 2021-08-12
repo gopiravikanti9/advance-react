7cce125f67f728fa5e24ef11071e720b3eca23e5
GOPICHAND Ravikanti <ravikanti.gopichand@soprasteria.com>
2021-08-12 12:06:27 +0530

    JSON server using axios

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import Forms from './tutorial/Forms';
-import UseEffectBasics from './tutorial/UseEffectBasics';
-
-
+import React from "react";
+import Forms from "./Tutorial/Forms";
+import LearnJSON from "./Tutorial/LearnJSON";
+import ToDo from "./Tutorial/ToDo";
+import UseEffectsBasics from "./Tutorial/UseEffectsBasics";
+import UseStateArray from "./Tutorial/UseStateArray";
+import UseStateBasic from "./Tutorial/UseStateBasic"; import UseStateCounter from "./Tutorial/UseStateCounter";
+import UseStateExample from "./Tutorial/useStateExample";
+import UseStateObject from "./Tutorial/UseStateObject";
 function App() {
-  return (<div className='container'>
+  return (
+    <div className="container">
+      <header className="App-header">
+        <h1>Advance React</h1>
+        {/* <UseStateExample/> */}
+        {/* <UseStateBasic/> */}
+        {/* <UseStateArray/> */}
+        {/* <UseStateObject/> */}
+        {/* <UseStateCounter/> */}
+        {/* <UseEffectsBasics/> */}
+        {/* <Forms/> */}
+        {/* <ToDo/> */}
 
-
-    <Forms />
-  </div>
+        <LearnJSON />
+      </header>
+    </div>
   );
 }
 
 export default App;
+
